22407863bc2e3516ca8aa90f67f1c9b6cea4c3a4
christiangv92 <christian.veiga92@gmail.com>
2023-06-13 16:49:24 -0300

    second commit - contador maior para menor

--- script.js
+++ script.js
@@ -6,14 +6,21 @@ function contar() {
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('ERRO: faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{27A1}`
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{27A1}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{27A1}`
+            }
         }
         res.innerHTML += `\u{1F6A9}`
     }
